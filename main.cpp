108f42ef2cae1ea637ee3ad0b504d71070cc010e
Razvanc1006 <razvan.ciomaga@yahoo.com>
2017-01-11 21:05:03 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,18 +5,19 @@ using namespace std;
 bool pierdut;
 const int lungime=20;
 const int latime=20;
-int dx,dy,fructdx,fructdy,scor;
-void Harta();
-void Initializare ();
-void Logica ();
-void Intrare();
-
-int main()
+int dx,dy,fructdx,fructdy,scor,coadaX[100],coadaY[100],coadaLG;
+enum directie {STOP=0,STANGA,DREAPTA,SUS,JOS};
+directie dir;
+void Initializare ()
 {
-    Harta();
-    Initializare();
+    pierdut=false;
+    dx=latime/2;
+    dy=lungime/2;
+    fructdx=rand()%latime;
+    fructdy=rand()%lungime;
+    scor=0;
 }
-void Harta()
+void Harta ()
 {
     system("cls");
     for(int i=0;i<latime+2;i++)
@@ -36,7 +37,20 @@ void Harta()
             else if(i==fructdy && j==fructdx)
                 cout<<"1";
             else
-            cout<<" ";
+            {    bool marireCoada=false;
+                for(int k=0;k<coadaLG;k++)
+                {
+                    if(coadaX[k]==j &&  coadaY[k]==i)
+                    {
+                        cout<<"s";
+                        marireCoada=true;
+
+                    }
+
+                }
+                if(!marireCoada)
+                  cout<<" ";
+            }
 
 
             if(j==latime-1)
@@ -49,21 +63,94 @@ void Harta()
 
     cout<<endl;
 
-}
-void Initializare()
-{
-     pierdut=false;
-    dx=latime/2;
-    dy=lungime/2;
-    fructdx=rand()%latime;
-    fructdy=rand()%lungime;
-    scor=0;
+    cout<<"Scor:"<<scor<<endl;
 }
 void Intrare()
 {
+   if(_kbhit())
+   {
+       switch(_getch())
+       {
+           case 'w':
+            dir=SUS;
+            break;
+           case 's':
+            dir=JOS;
+            break;
+           case 'a':
+            dir=STANGA;
+            break;
+           case 'd':
+            dir=DREAPTA;
+            break;
+           case 'k':
+            pierdut=true;
+            break;
+       }
+   }
+
 
 }
-void Logica()
-{
+void Logica ()
+{ int Xanterior=coadaX[0];
+  int Yanterior=coadaY[0];
+  int X2anterior,Y2anterior;
+  coadaX[0]=dx;
+  coadaY[0]=dy;
+  for(int i=1;i<coadaLG;i++)
+  {
+      X2anterior=coadaX[i];
+      Y2anterior=coadaY[i];
+      coadaX[i]=Xanterior;
+      coadaY[i]=Yanterior;
+      Xanterior=X2anterior;
+      Yanterior=Y2anterior;
+  }
+  switch(dir)
+  {
+      case SUS:
+          dy--;
+          break;
+      case JOS:
+          dy++;
+          break;
+      case STANGA:
+          dx--;
+          break;
+      case DREAPTA:
+          dx++;
+          break;
+      default:
+        break;
 
+  }
+ if(dx>latime || dx<0 || dy>lungime || dy<0 )
+    pierdut=true;
+  //if(dx >= latime) dx=0;
+  //else if (dx<0) dx=latime-1;
+  //if(dy >= lungime) dy=0;
+  //else if (dy<0) dy=lungime-1;
+  for(int i=0;i<coadaLG;i++)
+    if(coadaX[i]==dx && coadaY[i]==dy)
+     pierdut = true;
+  if(dx==fructdx && dy==fructdy)
+  {
+      coadaLG++;
+      scor++;
+      fructdx=rand()%latime;
+      fructdy=rand()%lungime;
+  }
+
+}
+int main()
+{
+    Initializare();
+    while(!pierdut)
+    {
+        Harta();
+        Intrare();
+        Logica();
+        Sleep(200);
+    }
+    return 0;
 }
